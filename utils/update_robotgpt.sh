790b7cf75a443f4cf66741d385f1c362432ca66a
ronypepper <80472924+ronypepper@users.noreply.github.com>
2026-07-12 14:54:26 +0200

    Added update script

---
+++ utils/update_robotgpt.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This script updates the RobotGPT repository from remote and subsequently the openpi repository with new files from
+# the RobotGPT repository. Assumes openpi to be installed next to the RobotGPT repository.
+
+# Change cwd to shell script directory regardless of where script was called from
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
+# Perform update
+cd ..
+git pull
+cp -r RobotGPT/openpi/robotgpt openpi/src
